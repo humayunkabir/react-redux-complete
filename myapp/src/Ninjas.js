4e31db3d1324227cb6b9a35eef12e6b654b87afd
Humayun Kabir <humayunkabir.cep@gmail.com>
2018-10-08 20:28:57 +0600

    Component

---
+++ myapp/src/Ninjas.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Ninjas extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="ninja">
+        <div>Name: Ryu</div>
+        <div>Age: 30</div>
+        <div>Belt: Black</div>
+      </div>
+    );
+  }
+}
+
+export default Ninjas;
