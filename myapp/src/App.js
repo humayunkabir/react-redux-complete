64609a2188d400af78eddfe34f89dcb0cd471446
Humayun Kabir <humayunkabir.cep@gmail.com>
2018-10-08 21:42:25 +0600

    Create From

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Ninjas from './Ninjas';
+import AddNinja from './AddNinja';
 
 class App extends Component {
   state = {
     ninjas: [
       { name: 'Ryu', age: 30, belt: 'black', id: 1 },
       { name: 'Yoshi', age: 20, belt: 'green', id: 2 },
-      { name: 'Crystal', age: 25, belt: 'pink', id: 3 },
+      { name: 'Crystal', age: 16, belt: 'pink', id: 3 },
       { name: 'Adam', age: 43, belt: 'yellow', id: 4 }
     ]
   };
@@ -17,6 +18,7 @@ class App extends Component {
         <h1>My first React app!</h1>
         <p>Welcome :)</p>
         <Ninjas ninjas={ninjas} />
+        <AddNinja />
       </div>
     );
   }
