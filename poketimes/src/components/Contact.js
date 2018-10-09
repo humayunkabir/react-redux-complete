4ee09738d90c66b2739ef5d6a1fe8e3c8e8c5e18
Humayun Kabir <humayunkabir.cep@gmail.com>
2018-10-09 20:38:18 +0600

    Programetic redirection

--- poketimes/src/components/Contact.js
+++ poketimes/src/components/Contact.js
@@ -1,12 +1,21 @@
 import React from 'react';
 
-const Contact = () => {
-    return (
-        <div className="container">
-            <h4 className="center">Contact</h4>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. In debitis commodi asperiores eum qui illo quos, cupiditate repudiandae? Recusandae expedita quas eius quis aut omnis dolore eveniet accusantium nemo dolores.</p>
-        </div>
-    )
-}
+const Contact = props => {
+  setTimeout(() => {
+    // props.history.push('/about');
+    console.log(props.history);
+  }, 2000);
+  return (
+    <div className="container">
+      <h4 className="center">Contact</h4>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. In debitis
+        commodi asperiores eum qui illo quos, cupiditate repudiandae? Recusandae
+        expedita quas eius quis aut omnis dolore eveniet accusantium nemo
+        dolores.
+      </p>
+    </div>
+  );
+};
 
 export default Contact;
--- poketimes/src/components/Navbar.js
+++ poketimes/src/components/Navbar.js
@@ -1,7 +1,11 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, withRouter } from 'react-router-dom';
 
-const Navbar = () => {
+const Navbar = props => {
+  setTimeout(() => {
+    props.history.push('/about');
+    console.log(props.history);
+  }, 2000);
   return (
     <nav className="nav-wrapper red darken-3">
       <div className="container">
@@ -26,4 +30,4 @@ const Navbar = () => {
   );
 };
 
-export default Navbar;
+export default withRouter(Navbar);
