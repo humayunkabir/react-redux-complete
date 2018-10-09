9013731e5dc2e1fe3db332ab9a2a2db31ec7ff24
Humayun Kabir <humayunkabir.cep@gmail.com>
2018-10-09 19:27:25 +0600

    React Router(poketimes)

---
+++ poketimes/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Navbar from './components/Navbar';
+// import {BrowserRouter} from 'react-router-dom';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ poketimes/src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className="container">
+            <h4 className="center">About</h4>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis explicabo quasi ex architecto, dolores ratione nesciunt voluptas asperiores perferendis sapiente. Rerum animi ea ad similique asperiores at aspernatur quibusdam dolor?</p>
+        </div>
+    )
+}
+
+export default About;
